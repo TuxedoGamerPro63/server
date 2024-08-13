c0ac6affa4c6097e46aa9b1d49f57f189653052b
TuxedoGamerPro63 <70236233+TuxedoGamerPro63@users.noreply.github.com>
2024-08-13 01:02:56 +0000

    Mundo guardado 2024-08-13

--- server.py
+++ server.py
@@ -1,6 +1,8 @@
 # Versiones disponibles
 # forge, mohist, fabric, vanilla, paper
 
+
+
 # Puedes instalar mohist después de instalar forge desde el menú de gestionar
 # Puedes instalar paper después de instalar vanilla desde el menú de gestionar
 # Puedes instalar purpur después de instalar fabric desde el menú de gestionar
@@ -16,6 +18,9 @@
 # sa		      Sudamérica (São Paulo)
 # us		      Estados unidos (Ohio)
 # us-cal-1	      Estados unidos (California)
+
+
+
 
 
 
